3f00b9acca6b09a39c1bbefc994797eab5f94b3d
suzhe111 <98070953+suzhe111@users.noreply.github.com>
2022-01-20 11:19:08 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,10 @@
 apt update
 apt install git zsh tmux fish
+
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+apt-get install docker-ce docker-ce-cli containerd.io
